04e665944ee8e0c95b7e10df82604e60374da0d3
Hanson Wong <Dismounted@users.noreply.github.com>
2017-08-04 22:15:02 +1000

    Better hover.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
+var step2Clicked = {
+    "Para1": false
+};
+
 $(function() {
     $("#Step1Continue").click(function() {
         $("#Step1").slideUp();
         $("#Step2").delay(750).slideDown();
     });
-    $("#Step2Para1").mouseover(function() {
-        $(this).popover({
-            title: "test",
-            content: "Test",
-            placement: "left"
+    $("#Step2Para1")
+        .mouseover(function() {
+            if (step2Clicked["Para1"] == false) {
+                $(this).children("span").css("background-color", "#f1d5f6");
+            }
+        })
+        .mouseout(function() {
+            if (step2Clicked["Para1"] == false) {
+                $(this).children("span").css("background-color", "inherit");
+            }
+        })
+        .click(function() {
+            step2Clicked["Para1"] = (step2Clicked["Para1"] ? false : true);
+            $("#Step2Para1Left").fadeToggle();
+            $("#Step2Para1Right").fadeToggle();
         });
-        $(this).popover("show");
-    });
-    $("#Step2Para1").mouseout(function() {
-        $(this).popover("hide");
-    });
+    $(".gutter-para")
+        .mouseover(function() {
+            $(this).fadeTo("fast", 1);
+        })
+        .mouseout(function() {
+            $(this).fadeTo("fast", 0.5);
+        });
 });
