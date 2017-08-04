98a4e509d81e536ae76050c87f4b74244ed72caf
Hanson Wong <Dismounted@users.noreply.github.com>
2017-08-04 21:35:29 +1000

    Simple hover.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 $(function() {
     $("#Step1Continue").click(function() {
         $("#Step1").slideUp();
-        $("#Step2").slideDown();
+        $("#Step2").delay(750).slideDown();
+    });
+    $("#Step2Para1").mouseover(function() {
+        $(this).popover({
+            title: "test",
+            content: "Test",
+            placement: "left"
+        });
+        $(this).popover("show");
+    });
+    $("#Step2Para1").mouseout(function() {
+        $(this).popover("hide");
     });
 });
