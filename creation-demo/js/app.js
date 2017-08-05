ae640fd221928e76260a9a6fcfbdecce02c51ba3
Hanson Wong <Dismounted@users.noreply.github.com>
2017-08-05 15:30:44 +1000

    Add optional provisions.

--- creation-demo/js/app.js
+++ creation-demo/js/app.js
@@ -19,6 +19,12 @@ var step2Clicked = {
     "Para6_3": false
 };
 var step2ClickInProgress = false;
+var step2EditorClicked = {
+    "Para_5_1_1": false,
+    "Para_5_1_2": false,
+    "Para_5_2_2": false,
+    "Para_5_2_3": false
+}
 
 $(function() {
     $("#Step1Continue").click(function() {
@@ -46,21 +52,6 @@ $(function() {
             if (step2Clicked["Para1_1"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para1_1"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para1_1"] = (step2Clicked["Para1_1"] ? false : true);
-            $("#Step2Para1_1Left").fadeToggle();
-            $("#Step2Para1_1Right").fadeToggle();
         });
     $("#Step2Para1_2 > h3")
         .mouseover(function() {
@@ -72,21 +63,6 @@ $(function() {
             if (step2Clicked["Para1_2"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para1_2"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para1_2"] = (step2Clicked["Para1_2"] ? false : true);
-            $("#Step2Para1_2Left").fadeToggle();
-            $("#Step2Para1_2Right").fadeToggle();
         });
     $("#Step2Para2_1 > h3")
         .mouseover(function() {
@@ -98,21 +74,6 @@ $(function() {
             if (step2Clicked["Para2_1"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para2_1"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para2_1"] = (step2Clicked["Para2_1"] ? false : true);
-            $("#Step2Para2_1Left").fadeToggle();
-            $("#Step2Para2_1Right").fadeToggle();
         });
     $("#Step2Para2_2 > h3")
         .mouseover(function() {
@@ -124,21 +85,6 @@ $(function() {
             if (step2Clicked["Para2_2"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para2_2"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para2_2"] = (step2Clicked["Para2_2"] ? false : true);
-            $("#Step2Para2_2Left").fadeToggle();
-            $("#Step2Para2_2Right").fadeToggle();
         });
     $("#Step2Para2_3 > h3")
         .mouseover(function() {
@@ -150,21 +96,6 @@ $(function() {
             if (step2Clicked["Para2_3"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para2_3"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para2_3"] = (step2Clicked["Para2_3"] ? false : true);
-            $("#Step2Para2_3Left").fadeToggle();
-            $("#Step2Para2_3Right").fadeToggle();
         });
     $("#Step2Para3_1 > h3")
         .mouseover(function() {
@@ -176,21 +107,6 @@ $(function() {
             if (step2Clicked["Para3_1"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para3_1"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para3_1"] = (step2Clicked["Para3_1"] ? false : true);
-            $("#Step2Para3_1Left").fadeToggle();
-            $("#Step2Para3_1Right").fadeToggle();
         });
     $("#Step2Para3_2 > h3")
         .mouseover(function() {
@@ -202,21 +118,6 @@ $(function() {
             if (step2Clicked["Para3_2"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para3_2"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para3_2"] = (step2Clicked["Para3_2"] ? false : true);
-            $("#Step2Para3_2Left").fadeToggle();
-            $("#Step2Para3_2Right").fadeToggle();
         });
     $("#Step2Para3_3 > h3")
         .mouseover(function() {
@@ -228,21 +129,6 @@ $(function() {
             if (step2Clicked["Para3_3"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para3_3"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para3_3"] = (step2Clicked["Para3_3"] ? false : true);
-            $("#Step2Para3_3Left").fadeToggle();
-            $("#Step2Para3_3Right").fadeToggle();
         });
     $("#Step2Para3_4 > h3")
         .mouseover(function() {
@@ -254,21 +140,6 @@ $(function() {
             if (step2Clicked["Para3_4"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para3_4"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para3_4"] = (step2Clicked["Para3_4"] ? false : true);
-            $("#Step2Para3_4Left").fadeToggle();
-            $("#Step2Para3_4Right").fadeToggle();
         });
     $("#Step2Para4_1 > h3")
         .mouseover(function() {
@@ -280,21 +151,6 @@ $(function() {
             if (step2Clicked["Para4_1"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para4_1"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para4_1"] = (step2Clicked["Para4_1"] ? false : true);
-            $("#Step2Para4_1Left").fadeToggle();
-            $("#Step2Para4_1Right").fadeToggle();
         });
     $("#Step2Para4_2 > h3")
         .mouseover(function() {
@@ -306,21 +162,6 @@ $(function() {
             if (step2Clicked["Para4_2"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para4_2"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para4_2"] = (step2Clicked["Para4_2"] ? false : true);
-            $("#Step2Para4_2Left").fadeToggle();
-            $("#Step2Para4_2Right").fadeToggle();
         });
     $("#Step2Para4_3 > h3")
         .mouseover(function() {
@@ -332,21 +173,6 @@ $(function() {
             if (step2Clicked["Para4_3"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para4_3"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para4_3"] = (step2Clicked["Para4_3"] ? false : true);
-            $("#Step2Para4_3Left").fadeToggle();
-            $("#Step2Para4_3Right").fadeToggle();
         });
     $("#Step2Para5_1 > h3")
         .mouseover(function() {
@@ -364,8 +190,8 @@ $(function() {
                 step2ClickInProgress = true;
                 $.each(step2Clicked, function(key, val) {
                     if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
+                        $("#Step2" + key + " > h3").click();
+                        $("#Step2" + key + " > h3").mouseout();
                     }
                 });
                 step2ClickInProgress = false;
@@ -390,8 +216,8 @@ $(function() {
                 step2ClickInProgress = true;
                 $.each(step2Clicked, function(key, val) {
                     if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
+                        $("#Step2" + key + " > h3").click();
+                        $("#Step2" + key + " > h3").mouseout();
                     }
                 });
                 step2ClickInProgress = false;
@@ -410,21 +236,6 @@ $(function() {
             if (step2Clicked["Para5_3"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para5_3"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para5_3"] = (step2Clicked["Para5_3"] ? false : true);
-            $("#Step2Para5_3Left").fadeToggle();
-            $("#Step2Para5_3Right").fadeToggle();
         });
     $("#Step2Para6_1 > h3")
         .mouseover(function() {
@@ -436,21 +247,6 @@ $(function() {
             if (step2Clicked["Para6_1"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para6_1"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para6_1"] = (step2Clicked["Para6_1"] ? false : true);
-            $("#Step2Para6_1Left").fadeToggle();
-            $("#Step2Para6_1Right").fadeToggle();
         });
     $("#Step2Para6_2 > h3")
         .mouseover(function() {
@@ -462,21 +258,6 @@ $(function() {
             if (step2Clicked["Para6_2"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para6_2"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para6_2"] = (step2Clicked["Para6_2"] ? false : true);
-            $("#Step2Para6_2Left").fadeToggle();
-            $("#Step2Para6_2Right").fadeToggle();
         });
     $("#Step2Para6_3 > h3")
         .mouseover(function() {
@@ -488,23 +269,34 @@ $(function() {
             if (step2Clicked["Para6_3"] == false) {
                 $(this).children("span").css("background-color", "inherit");
             }
-        })
-        .click(function() {
-            if (step2ClickInProgress == false && step2Clicked["Para6_3"] == false) {
-                step2ClickInProgress = true;
-                $.each(step2Clicked, function(key, val) {
-                    if (val == true) {
-                        $("#Step2" + key).click();
-                        $("#Step2" + key).mouseout();
-                    }
-                });
-                step2ClickInProgress = false;
-            }
-            step2Clicked["Para6_3"] = (step2Clicked["Para6_3"] ? false : true);
-            $("#Step2Para6_3Left").fadeToggle();
-            $("#Step2Para6_3Right").fadeToggle();
         });
+    $("#Step2Para5_1LeftAdd").click(function() {
+        $("#Step2Para5_1 > h3").click();
+        $("#Step2Para5_1 > h3").mouseout();
+        $("#Step2Para5_1_2").delay(750).slideDown();
+    });
     $("#Step2Para5_1_1 > .col-sm-11").click(function() {
-        $(this).children("div").trumbowyg();
+        if (step2EditorClicked["Para_5_1_1"] == false) {
+            $(this).children("div").trumbowyg();
+            step2EditorClicked["Para_5_1_1"] = true;
+        }
+    });
+    $("#Step2Para5_1_2 > .col-sm-11").click(function() {
+        if (step2EditorClicked["Para_5_1_2"] == false) {
+            $(this).children("div").trumbowyg();
+            step2EditorClicked["Para_5_1_2"] = true;
+        }
+    });
+    $("#Step2Para5_2_2 > .col-sm-11").click(function() {
+        if (step2EditorClicked["Para_5_2_2"] == false) {
+            $(this).children("div").trumbowyg();
+            step2EditorClicked["Para_5_2_2"] = true;
+        }
+    });
+    $("#Step2Para5_2_3 > .col-sm-11").click(function() {
+        if (step2EditorClicked["Para_5_2_3"] == false) {
+            $(this).children("div").trumbowyg();
+            step2EditorClicked["Para_5_2_3"] = true;
+        }
     });
 });
